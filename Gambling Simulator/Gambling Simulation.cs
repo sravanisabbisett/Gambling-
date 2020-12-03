542a776acd368cff244595af0619584d63a00248
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-12-03 09:55:58 +0530

    STARTED:Gambling simulation

---
+++ Gambling Simulator/Gambling Simulation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gambling_Simulator
+{
+    class Gambling_Simulation
+    {
+    }
+}
---
+++ Gambling Simulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gambling_Simulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
